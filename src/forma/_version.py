704c52f77e6b795f995bb640d3bf676268789bb7
Tiago Taveira <tiagotaveira@tms.lan>
2026-03-21 12:41:48 +0000

    Fix devcontainer: use Dockerfile.devcontainer, fix env_file path

--- src/forma/_version.py
+++ src/forma/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev0+d20260321'
-__version_tuple__ = version_tuple = (0, 1, 'dev0', 'd20260321')
+__version__ = version = '0.1.dev1+g7c0828154.d20260321'
+__version_tuple__ = version_tuple = (0, 1, 'dev1', 'g7c0828154.d20260321')
 
-__commit_id__ = commit_id = None
+__commit_id__ = commit_id = 'g7c0828154'
